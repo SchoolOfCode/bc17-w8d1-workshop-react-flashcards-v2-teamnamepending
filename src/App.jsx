702f961d382eb82c669e0985c658c3f6fa01d6b4
4rchie-r <archiereader@outlook.com>
2024-08-19 11:46:02 +0100

    header and footer linking

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
+import './App.css';
+import './components/Header/Header.css';
+import './components/Footer/Footer.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [flashcards, setFlashcards] = useState([]);
+
+  const addFlashcard = (flashcard) => {
+    setFlashcards([...flashcards, flashcard]);
+  };
 
   return (
-    <>
-    <Flashcard />
-    </>
-  )
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
